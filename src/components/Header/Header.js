2eea8c66cb4de2664efa01c8f71cb365750ff746
MunnaAhmed130 <munnaahmed2025@gmail.com>
2021-09-26 22:03:49 +0600

    commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className="">
+        <div className="header-info-container">
             <div className="header-info">
                 <h3 className="billionaire">Billionaire Club</h3>
                 <h3>Total Budget : <span className="budget">1000 Billion</span> </h3>
